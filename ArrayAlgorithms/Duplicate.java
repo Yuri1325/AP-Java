d115d23b9991b1565a7ce2b789083eaa10c24457
Uri <78451970+Yuri1325@users.noreply.github.com>
2026-01-22 12:44:48 -0500

    ------

--- ArrayAlgorithms/Duplicate.java
+++ ArrayAlgorithms/Duplicate.java
@@ -9,7 +9,7 @@ public class Duplicate {
     public static boolean containsDuplicate(String[] arr) {
         int i = 0;
         while (i < arr.length) {
-            for (int x = i + 1; i < arr[i].length(); x++)
+            for (int x = i + 1; x < arr.length; x++)
                 if (arr[i].equals(arr[x]))
                     return true;
             i++;
