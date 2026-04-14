34e85c952c90c9e86250a751469d0d1d32b75de0
Uri <78451970+Yuri1325@users.noreply.github.com>
2026-04-14 19:17:38 -0400

    --

--- ArrayList Algorithms/PraticeThree.java
+++ ArrayList Algorithms/PraticeThree.java
@@ -27,20 +27,46 @@ public class PraticeThree {
             int max = 0;
             int index = 0;
 
-            for(String[] row: arr)
+            for(int v =0; v<arr.length;v++)
                 {
                     int dupes = 0;
-                    for(int i =0;i<row.length;i++)
+                    for(int i =0;i<arr[v].length;i++)
                         {
-                            for(int j =i+1;j<row.length;j++)
+                            for(int j =i+1;j<arr[v].length;j++)
                                 {
-                                    if(row[i].equals(row[j]))
+                                    if(arr[v][i].equals(arr[v][j]))
                                         dupes++;
                                 }  
                         
                         }
-                    if
+                    if(dupes>max)
+                    {
+                        max = dupes;
+                        index = v;
+                    }
+                        
 
                  }
+            return index;
 
+}
+    // public static String[][] shift(String[][] arr)
+    //     {
+           
+    //         int numRow = arr.length;
+    //         int numColl = arr[0].length;
+    //         String[][] rArray = new String[numRow][numColl];
+    //         int counter = 0;
+
+    //        for(int i = numRow -1;i>0;i--)
+    //             {
+    //                 for(int j = numColl;j>0;j--)
+    //                 {
+    //                     arr[i].
+    //                     counter++;
+    //                 }
+    //             }
+    //     }
+    //Very confused on how this possible u cant add or shits elements
+    //  in arrays it used 1 index  the previous element there will be removed
 }
--- FRQ Practice/Feeder.java
+++ FRQ Practice/Feeder.java
@@ -2,11 +2,34 @@ public class Feeder {
 
     private int currentFood;
 
-    public Feeder(int initialFood) {
+    public Feeder(int initialFood) 
+        {
         currentFood = initialFood;
-    }
+        }  
 
-    public void simulateOneDay(int numBirds) {
-
-    }
+    public void simulateOneDay(int numBirds) 
+        {
+            int bear = (int)(Math.random()*(100+1));
+            if(bear<5)
+                currentFood = 0;
+            else
+                {   
+                    for(int i = 0; i<=numBirds;i++)   
+                        currentFood -= (int)(Math.random()*(50-11)+10);
+                }
+                    
+            currentFood = (currentFood>0) ? currentFood : 0;
+        }
+    public int simulatemManyDays(int numBirds,int numDays)
+        {
+            int count = 0;
+            for(int i =0; i<=numDays;i++)
+                {
+                    if(currentFood == 0)
+                        break;
+                    simulateOneDay(numBirds);
+                    count++;
+                }
+            return count;
+        }
 }
